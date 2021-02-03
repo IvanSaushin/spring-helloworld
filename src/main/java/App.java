de0874766062a89be3a4d7efcf9b6a24ba787d1a
Ivan Saushin <ivan.saushin@gmail.com>
2021-02-04 00:46:20 +0400

    my first commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,18 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        boolean beanComparion = bean1.equals(bean2);
+        boolean catComparion = cat1.equals(cat2);
+
+        System.out.println(beanComparion);
+        System.out.println(catComparion);
+
+        System.out.println(bean1.getMessage());
     }
 }
